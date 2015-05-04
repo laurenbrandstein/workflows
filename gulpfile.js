2cd3f292ccb31b02d51bf65d8c3714bfd3c17d2d
Lauren Brandstein <lbrandstein@inrhythm.com>
2015-05-04 10:08:57 -0400

    Add coffeescript files.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,16 @@
 var gulp = require('gulp'),
-	gutil = require('gulp-util');
+	gutil = require('gulp-util'),
+	coffee = require('gulp-coffee');
+
+var coffeeSrcs = ['components/coffee/tagline.coffee'];
 
 gulp.task('log', function() {
 	gutil.log('Workflows are awesome!');
+});
+
+gulp.task('coffee', function() {
+	gulp.src(coffeeSrcs)
+		.pipe(coffee({ bare: true })
+			.on('error', gutil.log))
+		.pipe(gulp.dest('components/scripts'))
 });
