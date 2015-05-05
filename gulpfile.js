e6f867311c04d6bdc58aca8455370463103c4f11
Lauren Brandstein <lbrandstein@inrhythm.com>
2015-05-05 10:56:01 -0400

    Add live reloading.

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@
 var browserify = require('gulp-browserify'),
     compass = require('gulp-compass'),
     concat = require('gulp-concat'),
+    connect = require('gulp-connect'),
     coffee = require('gulp-coffee'),
     gulp = require('gulp'),
     gutil = require('gulp-util');
@@ -47,7 +48,8 @@ gulp.task('js', function () {
     gulp.src(jsSources)
         .pipe(concat('script.js'))
         .pipe(browserify())
-		.pipe(gulp.dest('builds/development/js'));
+        .pipe(gulp.dest('builds/development/js'))
+        .pipe(connect.reload());
 });
 
 gulp.task('compass', function () {
@@ -64,16 +66,25 @@ gulp.task('compass', function () {
             }
         ))
             .on('error', gutil.log)
-        .pipe(gulp.dest('builds/development/css'));
+        .pipe(gulp.dest('builds/development/css'))
+        .pipe(connect.reload());
 });
 
-gulp.task('watch', function(){
+gulp.task('watch', function () {
     gulp.watch(coffeeSources, ['coffee']);
     gulp.watch(jsSources, ['js']);
     gulp.watch(sassSourcesAll, ['compass']);
 });
 
 
+gulp.task('connect', function () {
+    connect.server({
+        livereload: true,
+        root: 'builds/development/'
+    });
+});
+
+
 // *** Default ***
 
-gulp.task('default', ['coffee', 'js', 'compass', 'watch']);
+gulp.task('default', ['coffee', 'js', 'compass', 'connect', 'watch']);
